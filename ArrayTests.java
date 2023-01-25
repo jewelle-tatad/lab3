7efae44bd3b47722c6ffb85d9649f604b2858a27
jewelle-tatad <jewelle.tatad@gmail.com>
2023-01-25 11:36:05 -0800

    added tests for arrayTests and fixed bugs

--- ArrayTests.java
+++ ArrayTests.java
@@ -2,6 +2,7 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class ArrayTests {
+  
 	@Test 
 	public void testReverseInPlace() {
     int[] input1 = { 3 };
@@ -14,5 +15,18 @@ public class ArrayTests {
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReverseInPlace2() {
+    int[] input1 = {1, 2, 3, 4};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{4, 3, 2, 1}, input1);
+  }
+
+  @Test
+  public void testReversed2() {
+    int[] input1 = {1, 2, 3, 4};
+    assertArrayEquals(new int[]{4, 3, 2, 1}, ArrayExamples.reversed(input1));
   }
 }
